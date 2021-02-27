653c48ae41ea08e26093e8a29159e8919dc0dddf
Prathamesh Mandke <mandkepk97@gmail.com>
2021-02-27 12:00:56 -0500

    initial commit

---
+++ misc/travel/india/state_data.js
@@ -0,0 +1,37 @@
+var states = {
+	KA: {
+		name: "Karnataka",
+		lived: "2017",
+		description: "Visit Sampanna at Flipkart, Bangalore.<br>Family trip at Mysore."
+	},
+	MH: {
+		name: "Maharashtra",
+		lived: "1997 - 2019",
+    		description: "Home"
+	},
+	RJ: {
+    		name: "Rajasthan",
+    		visited: "2011",
+    		description: "Family Trip at Jaipur"
+  	},
+	TN: {
+		name: "Tamil Naidu",
+		visited: "2016, 2019",
+    		description: "Family trip at Ooty.<br>Visa interview in Chennai."
+	},
+  	UP: {
+    		name: "Uttar Pradesh",
+    		visited: "2018",
+    		description: "Noida - IACC paper presentation in Noida.<br>Agra - Trip with friends."
+  	},
+	DL: {
+		name: "Delhi NCR",
+		visited: "2018",
+		description: "Trip with friends"
+	},
+	TG: {
+	 	name: "Telangana",
+	 	visited: "Feb 2019",
+	 	description: "Flew to/from Pune and Hyderabad in a 4-seater private Cessna to attend LARC'19!"
+ 	}
+}
---
+++ misc/travel/us/state_data.js
@@ -0,0 +1,32 @@
+var states = {
+  	CA: {
+    	name: "California",
+    	lived: "Dec'19 - Jan'20",
+    	description: "Visit Samruddhi in SoCal.<br>Visited San Diego, Los Angeles, Pasadena, Irvine, Laguna Hills and most beaches along the Pacific Coast Highway!"
+  	},
+  	DC2: {
+    		name: "Washington, District of Columbia",
+    		lived: "Aug'19, Dec'19",
+    		description: "Visit Sampanna@Reston.<br>Visit Union Station & Massachussetts Ave."
+	},
+	MD: {
+		name: "Maryland",
+		visited: "12/14/2019",
+		description: "Visit Chinmay in College Park.<br>Visit Baltimore (BWI)."
+	},
+	VA: {
+		name: "Virginia",
+		lived: "2019-Present",
+    		description: "Virginia Tech"
+	},
+	NC: {
+		name: "North Carolina",
+		visited: "6/13/2020",
+		description: "Visit to Smoky Mountains."
+	},
+	TN: {
+		name: "Tennessee",
+		visited: "6/13/2020",
+		description: "Visit to Gatlinburg and Sevierville in TN.<br>Visit to Clingman's Dome and Charlie's Bunion in the Smokies."
+	}
+};
