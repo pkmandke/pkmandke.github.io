279c0e283a2b4f58b64cbf9157639d825601bf24
Prathamesh Mandke <pkmandke@vt.edu>
2022-02-13 03:37:45 -0500

    update index.html; add places visited in US; change color scheme in map

--- misc/travel/us/state_data.js
+++ misc/travel/us/state_data.js
@@ -1,8 +1,8 @@
 var states = {
   	CA: {
     	name: "California",
-    	lived: "Dec'19 - Jan'20",
-    	description: "Visit Samruddhi in SoCal.<br>Visited San Diego, Los Angeles, Pasadena, Irvine, Laguna Hills and most beaches along the Pacific Coast Highway!"
+    	lived: "Dec'19 - Jan'20, Jul'21 - Present",
+    	description: "Visit Samruddhi in SoCal.<br>San Diego, Los Angeles, Pasadena, Irvine, Laguna Hills"
   	},
   	DC2: {
     		name: "Washington, District of Columbia",
@@ -28,5 +28,40 @@ var states = {
 		name: "Tennessee",
 		visited: "6/13/2020",
 		description: "Visit to Gatlinburg and Sevierville in TN.<br>Visit to Clingman's Dome and Charlie's Bunion in the Smokies."
-	}
+	},
+	FL: {
+		name: "Florida",
+		visited: "June 2020",
+		description: "Orlando, Daytona Beach, Miami, Key West"
+	},
+	WV: {
+		name: "West Virginia",
+		visited: "May/June 2020",
+		description: "Road Trip"
+	},
+	PA: {
+		name: "Pennsylvania",
+		visited: "May/June 2020",
+		description: "Road Trip"
+	},
+	NY: {
+		name: "New York",
+		visited: "June 2020",
+		description: "Road trip - Niagara Falls, NYC"
+	},
+	WA: {
+		name: "Washington",
+		visited: "Dec 2021",
+		description: "Visit Sam in Seattle"
+	},
+	NV: {
+		name: "Nevada",
+		visited: "Nov 2021",
+		description: "Road trip - SD to Vegas"
+	},
+	UT: {
+		name: "Utah",
+		visited: "Nov 2021",
+		description: "Zion and Bryce NP"
+	},
 };
