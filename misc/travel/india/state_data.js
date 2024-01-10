b1d9f4c6280bf5adc9d33e350c0802ea365bcc03
Prathamesh Mandke <pkmandke@vt.edu>
2024-01-10 08:31:01 -0800

    add KL trip info
    Signed-off-by: Prathamesh Mandke <pkmandke@vt.edu>

--- misc/travel/india/state_data.js
+++ misc/travel/india/state_data.js
@@ -1,28 +1,28 @@
 var states = {
 	KA: {
 		name: "Karnataka",
-		lived: "2017",
+		visited: "2017",
 		description: "Visit Sampanna at Flipkart, Bangalore.<br>Family trip at Mysore."
 	},
 	MH: {
 		name: "Maharashtra",
 		lived: "1997 - 2019",
-    		description: "Home"
+		description: "Home"
 	},
 	RJ: {
-    		name: "Rajasthan",
-    		visited: "2011",
-    		description: "Family Trip at Jaipur"
+		name: "Rajasthan",
+		visited: "2011",
+		description: "Family Trip at Jaipur"
   	},
 	TN: {
 		name: "Tamil Naidu",
 		visited: "2016, 2019",
-    		description: "Family trip at Ooty.<br>Visa interview in Chennai."
+		description: "Family trip at Ooty.<br>Visa interview in Chennai."
 	},
   	UP: {
-    		name: "Uttar Pradesh",
-    		visited: "2018",
-    		description: "Noida - IACC paper presentation in Noida.<br>Agra - Trip with friends."
+		name: "Uttar Pradesh",
+		visited: "2018",
+		description: "Noida - IACC paper presentation in Noida.<br>Agra - Trip with friends."
   	},
 	DL: {
 		name: "Delhi NCR",
@@ -33,5 +33,10 @@ var states = {
 	 	name: "Telangana",
 	 	visited: "Feb 2019",
 	 	description: "Flew to/from Pune and Hyderabad in a 4-seater private Cessna to attend LARC'19!"
- 	}
+ 	},
+	KL: {
+		name: "Kerala",
+		visited: "Dec 2023",
+		description: "Family Trip"
+	}
 }
